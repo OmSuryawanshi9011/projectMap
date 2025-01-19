2cfbc0c751d9d693ba6f7699f9093a1982b61479
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-19 08:12:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,45 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map((employee) => {
+    if(employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((employee) => {
+    if(employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "Tony", age: 30, profession: "tester" };
+  arr.push(newEmployee);
+  console.log(arr)
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  arr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  cons newArr = [
+    { id: 5, name: "Jasmin", age: 24, profession: "designer" },
+    { id: 6, name: "Thor", age: 35, profession: "hacker" },
+    { id: 7, name: "Venom", age: 21, profession: "manager" }
+    ];
+  const concatenatedArray = arr.concat(newArr);
+  console.log(concatenatedArray);
 }
+
+PrintDeveloperbyMap();
+PrintDeveloperbyForEach();
+addData();
+removeAdmin();
+concatenateArray();
+
